45037b1f86d373418952f183338aeee8fd6e9ff3
gildong <seolhyeon67@naver.com>
2024-11-27 09:16:39 +0900

    current

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function main() {
+    return (
+        <a href='https://web102f.netlify.app/'>
+            <img src='currentmain.png'/>
+        </a>
+    )
+}
+
+export default main;
